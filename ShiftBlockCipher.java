a723e1b2ca79c7c25fce30b4ca418aff36d28ae1
MuffinTheMan <larsenc91@gmail.com>
2017-06-02 21:53:55 -0600

    Split encrypt/decrypt into separate functions.

--- ShiftBlockCipher.java
+++ ShiftBlockCipher.java
@@ -1,6 +1,6 @@
 /*
     This is perhaps the simpliest block cipher. Each block is of length (t) '1' meaning each character is
-    shifted 'e' characters to the right
+    shifted 'e' (user supplied) characters to the right
 */
 
 import java.util.Scanner;
@@ -11,23 +11,34 @@ public class ShiftBlockCipher {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("What is your shift value? ");
-        Integer e = scanner.nextInt();
+        int e = scanner.nextInt();
 
         System.out.print("Enter your plain text message: ");
-        String s = scanner.next();
-        char[] chars = s.toCharArray();
+        String m = scanner.next();
+
+        String c = encrypt(m,e);
+
+        System.out.println("Encrypted message: " + c);
+        System.out.println("Plain text message: " + decrypt(c,e));
+    }
+
+    public static String encrypt(String m, int e) {
+        char[] chars = m.toCharArray();
 
         for (int i = 0; i < chars.length; i++) {
             chars[i] += e;
         }
 
-        System.out.println("Encrypted message: " + String.valueOf(chars));
+        return String.valueOf(chars);
+    }
+
+    public static String decrypt(String c, int e) {
+        char[] chars = c.toCharArray();
 
         for (int i = 0; i < chars.length; i++) {
             chars[i] -= e;
         }
 
-        System.out.println("Plain text message: " + String.valueOf(chars));
+        return String.valueOf(chars);
     }
-
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Cryptographic Exercises
 
 Simple implementations of select methods of cryptographic schemes to facilitate understanding various principles of cryptography.
+
+Forgive my code oddities. Java is not in my top 5 go-to languages, and I'm using it here to get more exposure to and practice with the language.
